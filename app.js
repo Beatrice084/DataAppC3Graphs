bdcd3e5b44477e9b9097fcad1d05d843785cf394
Beatrice084 <bjohn084@uottawa.ca>
2018-05-30 14:06:09 -0400

    Fixed day/month issue

--- app.js
+++ app.js
@@ -1,13 +1,4 @@
-console.log('something supersuper terrible');
 var express = require('express');
 var app = express();
-
-
-app.use('*', (req, res, next) => {
-    console.log(req.originalUrl);
-    console.log('efwgfwse')
-    next();
-});
-
 app.use(express.static('./'));
-app.listen(8080);
+app.listen(7070);
