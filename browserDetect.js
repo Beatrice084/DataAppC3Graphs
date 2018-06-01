965bfd66fff32bc132a161b07ba2c82b8d2409f4
Omar Radwan Mohsen <oradw074@uottawa.ca>
2018-06-01 09:22:09 -0400

    added browser detection and alert

---
+++ browserDetect.js
@@ -0,0 +1,6 @@
+// Internet Explorer 6-11
+var isIE = /*@cc_on!@*/false || !!document.documentMode;
+
+if (isIE == true){
+    alert("Please note that Internet Explorer 11 and below are no longer supported. We recommed upgrading to the latest Microsoft Edge, Google Chrome, or Firefox");
+}
